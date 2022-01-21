b19f806fa05a2ad81d7eba41d45ef661eb63f328
Bahadır Ünal <92628464+ZeroToHero2@users.noreply.github.com>
2022-01-21 21:02:21 +0300

    Update TrieST.java

--- Homework5/src/TrieST.java
+++ Homework5/src/TrieST.java
@@ -1,9 +1,6 @@
 //-----------------------------------------------------
 // Title: Class TrieST
-// Author: Abdusselam koç,Bahadır ünal
-// ID: 4931214741,1000776827
-// Section: 2,3 
-// Assignment: 5
+// Author: Abdusselam Koç,Bahadır Ünal
 // Description: The one of the Fundamental aspects for this task is “Trie Data Structure”.
 // This class used to store the” String stream” in an efficient way which is Linear Time (for most of the operations).
 //-----------------------------------------------------
